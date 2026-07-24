9dee08943d2733da915f04e808f225ce033a01d5
huanghao <374935367@qq.com>
2026-07-24 13:06:10 +0800

    chore: harden workspace quality gates

--- ui/vitest.workspace.ts
+++ ui/vitest.workspace.ts
@@ -1,3 +1,6 @@
 import { defineWorkspace } from 'vitest/config'
 
-export default defineWorkspace(['packages/*/vitest.config.ts'])
+export default defineWorkspace([
+  'apps/*/vitest.config.ts',
+  'packages/*/vitest.config.ts',
+])
