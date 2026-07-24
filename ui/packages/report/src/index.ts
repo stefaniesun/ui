b6a39b30fa8aef67627fbf4d49a10efebcbee92c
huanghao <374935367@qq.com>
2026-07-24 16:14:42 +0800

    feat: add semantic visual review reports

---
+++ ui/packages/report/src/index.ts
@@ -0,0 +1,2 @@
+export * from './feedback.js'
+export * from './generate-report.js'
---
+++ ui/packages/report/src/templates/report.html.ts
@@ -0,0 +1,2 @@
+export function html(value:unknown):string{return String(value).replaceAll('&','&amp;').replaceAll('<','&lt;').replaceAll('>','&gt;').replaceAll('"','&quot;').replaceAll("'",'&#39;')}
+export function jsonScript(value:unknown):string{return JSON.stringify(value).replaceAll('<','\\u003c').replaceAll('>','\\u003e').replaceAll('&','\\u0026').replaceAll('\u2028','\\u2028').replaceAll('\u2029','\\u2029')}
